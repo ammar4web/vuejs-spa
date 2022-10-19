23b29e3dd0ff00b3419d1fdb5743d6fed41eef0d
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-10-19 13:50:41 +0300

    Sidebar part1: the view

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,71 @@
         <span>Past Orders</span>
       </router-link>
     </nav>
-    <!-- <router-link @click="toggleSidebar" class="top-bar-cart-link">
-      <i class="icofont-cart-alt icofont-1x"></i> -->
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
       <!-- الرقم جانب عربة التسوق (عدد المنتجات) -->
-      <!-- <span>Cart ({{ totalQuantity }})</span>
-    </router-link> -->
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
   </header>
   <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav> -->
-  <router-view/>
+  <router-view :inventory="inventory" />
+  <SidebarComponent
+        v-if="showSidebar"
+        :toggle="toggleSidebar"
+        :cart="cart"
+        :inventory="inventory"
+        :remove="removeItem"
+        />
 </template>
+
+<script>
+import SidebarComponent from '@/components/SidebarComponent.vue'
+import food from './food.json'
+
+export default {
+  components: {
+    SidebarComponent
+  },
+  data () {
+    return {
+      showSidebar: true,
+      inventory: food,
+      cart: {}
+    }
+  },
+
+  //
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  //
+  methods: {
+    addToCart (name, index) {
+      console.log(name, index)
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += this.inventory[index].quantity
+      // تصفير مربع إدخال كمية المنتج
+      this.inventory[index].quantity = 0
+      console.log(this.cart)
+    },
+    //
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    //
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
 
 <style></style>
 <!--
