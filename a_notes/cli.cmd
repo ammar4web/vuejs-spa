39f31b0767ecf325b0db2e9e5f7deac144e9bf01
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2022-10-19 08:59:50 +0300

    PastOrders page and routing for it
    and using our style
    
    and empty the home page from the default vuejs content

--- a_notes/cli.cmd
+++ a_notes/cli.cmd
@@ -1,2 +1,8 @@
 vue create free-code-camp
 npm run serve
+
+@REM 
+@REM now these have a problem
+npm install -D sass-loader@^10 sass
+npm install -D sass-loader node-sass
+npm i sass-loader
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 
 //
-import './assets/test_styles/main.css'
+// import './assets/test_styles/main.css'
+//
+// we have a problem with sass loader
+// import './assets/styles/style.scss'
+//
+import './assets/styles/style.min.css'
 
 createApp(App).use(router).mount('#app')
